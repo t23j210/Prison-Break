d9975defe9e15b15cc11d1a29095b88066923045
t23j210 <t23j210ol@ous.jp>
2025-11-10 13:50:43 +0900

    Add close safe image to Jail room
    Load and display a close safe image in the Jail room.

--- jail.py
+++ jail.py
@@ -12,9 +12,12 @@ class Jail(Room_Base):
     def __init__(self, screen, lock_flag):
         super().__init__(screen, lock_flag)
         self.img_room = pygame.image.load("res/jail/jail.png")
+        self.close_safe = pygame.image.load("res/jail/zoom_under_bed(2)")
         
     def click_event(self, x, y):
         pass
             
     def draw(self):
         self.screen.blit(self.img_room, (0, 0))
+        if self.lock_flag[0] == False:
+            self.lock_screen.blit(self.close_safe, [440,213])
