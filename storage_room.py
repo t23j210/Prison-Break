aaf7fc847c5a0987b49d1cbd83096d139cbfecf1
P4skql <t23j177oc@ous.jp>
2025-11-10 15:29:47 +0900

    storage_room

--- storage_room.py
+++ storage_room.py
@@ -11,10 +11,23 @@ from room_base import Room_Base
 class Storage_room(Room_Base):
     def __init__(self, screen, lock_flag):
         super().__init__(screen, lock_flag)
-        self.img_room = pygame.image.load("res/storage_room/storage.png")
+        self.img_room1 = pygame.image.load("res/storage_room/storage_room.png")
+        self.img_room2 = pygame.image.load("res/storage_room/storage_room_no.png")
+        self.zoom_door = pygame.image.load("res/storage_room/zoom_door1.png")
+        self.zoom_state = 0
         
     def click_event(self, x, y):
-        pass
+        if self.zoom_state == 0:
+            if (490 < x < 615) and (260 < 390):
+                self.zoom_state = 1
+                
+        else:
+            is_inside = (170 < x <890) and (10 < y <750)
+            if not is_inside:
+                self.zoom_state = 0
             
     def draw(self):
-        self.screen.blit(self.img_room, (0, 0))
+       if self.zoom_state == 0:
+           self.screen.blit(self.img_room1, (0,0))
+       else:
+           self.screen.blit(self.zoom_door, (0,0))
