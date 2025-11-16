e6a473d5baff0728749815f7c54ee7063ac55613
P4skql <t23j177oc@ous.jp>
2025-11-16 23:25:53 +0900

    kitchen

--- kitchen.py
+++ kitchen.py
@@ -14,20 +14,21 @@ class Kitchen(Room_Base):
         self.img_room = pygame.image.load("res/kitchen/kitchen.png")
         self.zoom_refrigerator = pygame.image.load("res/kitchen/zoom_refrigerator.png")
         self.item_battery = pygame.image.load("res/kitchen/item_battery.png")
+        self.item_page2 = pygame.image.load("res/kitchen/item_page2.png")
         self.zoom_state = 0
         
     def click_event(self, x, y):
         if self.zoom_state == 0:
             if (90 < x < 170) and (265 < y < 405):
                 self.zoom_state = 1
-                
-        else:
+            else:
+                self.zoom_state = 0
+        elif self.zoom_state == 1:
             is_inside = (333 < x <685) and (55 < y <640)
             if not is_inside:
                 self.zoom_state = 0
             
     def draw(self):
-        if self.zoom_state == 0:
-           self.screen.blit(self.img_room, (0,0))
-        else:
-            self.screen.blit(self.zoom_refrigerator, (0,0))
+        self.screen.blit(self.img_room, (0,0))
+        if self.zoom_state == 1:
+           self.screen.blit(self.zoom_refrigerator, (0,0))
