9ecc649faf54648b91d1c08decb5f0b15a4e1516
P4skql <t23j177oc@ous.jp>
2025-11-16 23:25:44 +0900

    jail

--- jail.py
+++ jail.py
@@ -12,33 +12,36 @@ class Jail(Room_Base):
     def __init__(self, screen, lock_flag):
         super().__init__(screen, lock_flag)
         self.img_room = pygame.image.load("res/jail/jail.png")
+        self.img_jail = pygame.image.load("res/jail/jail_no.png")
         self.zoom_under_bed = pygame.image.load("res/jail/zoom_under_bed.png")
         self.zoom_key = pygame.image.load("res/jail/zoom_key.png")
         self.close_safe = pygame.image.load("res/jail/zoom_under_bed(2).png")
+        self.zoom_toilet = pygame.image.load("res/jail/zoom_toiret.png")
+        self.item_diary = pygame.image.load("res/jail/item_diary.png")
+        self.item_key = pygame.image.load("res/jail/item_key.png")
+        self.item_page = pygame.image.load("res/jail/item_page.png")
         self.zoom_state = 0
         
     def click_event(self, x, y):
         if self.zoom_state == 0:
-            if 555 < x < 650 and 580 < y < 750:
+            if (555 < x < 650) and (580 < y < 750):
                 self.zoom_state = 1
-            elif 240 < 300 and 520 < y < 650:
+            elif (240 < x < 300) and (520 < y < 650):
                 self.zoom_state = 2
             else:
                 self.zoom_state = 0
         elif self.zoom_state == 1:
-            if 100 < x < 200 and 100 < y < 200:
-                self.zoom_state = 1
-            else:
+            is_inside = (162 < x < 918) and (324 < y < 716)
+            if not is_inside:
                 self.zoom_state = 0
         elif self.zoom_state == 2:
-            if 100 < x < 200 and 100 < y < 200:
-                self.zoom_state = 2
-            else:
+            is_inside = (333 < x < 675) and (182 < y < 710)
+            if not is_inside:
                 self.zoom_state = 0
-                
+
     def draw(self):
-        self.screen.blit(self.img_room, (0, 0))
+        self.screen.blit(self.img_room, (0,0))
         if self.zoom_state == 1:
-            self.screen.blit(self.zoom_under_bed, (0, 0))
+            self.screen.blit(self.zoom_under_bed, (0,0))
         elif self.zoom_state == 2:
-            self.screen.blit(self.zoom_key, (0, 0))
+            self.screen.blit(self.zoom_key, (0,0))
