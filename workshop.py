e054267e80f2c3df1697b5a31224dca55b1e05e3
P4skql <t23j177oc@ous.jp>
2025-11-16 23:26:21 +0900

    workshop

--- workshop.py
+++ workshop.py
@@ -12,16 +12,46 @@ class Workshop(Room_Base):
     def __init__(self, screen, lock_flag):
         super().__init__(screen, lock_flag)
         self.img_room = pygame.image.load("res/workshop/workshop.png")
-        self.zoom_drill_no_needlever.png = pygame.image.load("res/workshop/zoom_drill_no_needlever.png")
-        self.zoom_no_drillver.png = pygame.image.load("res/workshop/zoom_no_drillver.png")
-        self.zoom_needle.png = pygame.image.load("res/workshop/zoom_needle.png")
-        self.zoom_no_needlever.png = pygame.image.load("res/workshop/zoom_no_needlever.png")
-        self.zoom_board.png = pygame.image.load("res/workshop/zoom_board.png")
+        self.zoom_drill_no_needlever = pygame.image.load("res/workshop/zoom_drill_no_needlever.png")
+        self.zoom_no_drillver = pygame.image.load("res/workshop/zoom_no_drillver.png")
+        self.zoom_needle = pygame.image.load("res/workshop/zoom_needle.png")
+        self.zoom_no_needlever = pygame.image.load("res/workshop/zoom_drill_no_needlever.png")
+        self.zoom_board = pygame.image.load("res/workshop/zoom_board.png")
+        self.zoom_board2 = pygame.image.load("res/workshop/zoom_board2.png")
+        self.zoom_page1 = pygame.image.load("res/workshop/zoom_page1.png")
+        self.item_drill = pygame.image.load("res/workshop/item_drill.png")
+        self.item_map = pygame.image.load("res/workshop/item_map.png")
+        self.item_nopage1ver = pygame.image.load("res/workshop/item_nopage1ver.png")
+        self.item_page1 = pygame.image.load("res/workshop/item_page1.png")
+        self.zoom_state = 0
         
     def click_event(self, x, y):
-        pass
-            
+        if self.zoom_state == 0:
+            if (763 < x < 841) and (313 < y < 347):
+                self.zoom_state = 1
+            elif (256 < x < 350) and (244 < y < 338):
+                self.zoom_state = 2
+            elif (896 < x < 989) and (210 < y < 989):
+                self.zoom_state = 3
+        elif self.zoom_state == 1:
+            is_inside = (433 < x < 940) and (422 < y < 719)
+            if not is_inside:
+                self.zoom_state = 0
+        elif self.zoom_state == 2:
+            is_inside = (380 < x < 680) and (527 < y < 720)
+            if not is_inside:
+                self.zoom_state = 0
+        elif self.zoom_state == 3:
+            is_inside = (284 < x < 721) and (148 < y < 543)
+            if not is_inside:
+                self.zoom_state = 0
+                      
     def draw(self):
         self.screen.blit(self.img_room, (0, 0))
-        if self.lock_flag[3] == False:
-            slef.screen.blit(slef.close_safe, [440, 213])
+        if self.zoom_state == 1:
+            self.screen.blit(self.zoom_drill_no_needlever, (0, 0))
+        elif self.zoom_state == 2:
+            self.screen.blit(self.zoom_needle, (0, 0))
+        elif self.zoom_state == 3:
+            self.screen.blit(self.zoom_board, (0, 0))
+        
