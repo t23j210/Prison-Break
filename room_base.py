e74523e5b0a54f9b935c066c15335409de383417
P4skql <t23j177oc@ous.jp>
2025-11-17 14:09:53 +0900

    room_base

--- room_base.py
+++ room_base.py
@@ -6,9 +6,11 @@ Created on Wed Dec  9 10:23:51 2020
 """
 
 class Room_Base:
-    def __init__(self, screen, lock_flag):
+    def __init__(self, screen, lock_flag, item_get, item_use):
         self.screen = screen
         self.lock_flag = lock_flag
+        self.item_get = item_get
+        self.item_use = item_use
     
     
     def pos_event(self, x, y):
@@ -16,6 +18,10 @@ class Room_Base:
     
     
     def click_event(self, x, y):
+        pass
+    
+    
+    def key_event(self, key):
         pass
     
     
