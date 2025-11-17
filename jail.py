8496fa40065e2ba8a3036a653309115938a4b796
t23j210 <t23j210ol@ous.jp>
2025-11-17 15:51:43 +0900

    jail

--- jail.py
+++ jail.py
@@ -21,6 +21,7 @@ class Jail(Room_Base):
         self.item_key = pygame.image.load("res/jail/item_key.png")
         self.item_page = pygame.image.load("res/jail/item_page.png")
         self.zoom_state = 0
+        self.item_sdriver_state = 0
         
     def click_event(self, x, y):
         if self.zoom_state == 0:
@@ -28,6 +29,8 @@ class Jail(Room_Base):
                 self.zoom_state = 1
             elif (240 < x < 300) and (520 < y < 650):
                 self.zoom_state = 2
+            elif self.item_get[1] == True:
+                self.zoom_state = 3
             else:
                 self.zoom_state = 0
         elif self.zoom_state == 1:
@@ -35,13 +38,21 @@ class Jail(Room_Base):
             if not is_inside:
                 self.zoom_state = 0
         elif self.zoom_state == 2:
-            is_inside = (333 < x < 675) and (182 < y < 710)
-            if not is_inside:
+            if (478 < x < 539) and (470 < y < 604):
+                self.item_sdriver_state = 1
+                self.item_get[1] = True
+                self.zoom_state = 3
+            else:
+                self.item_sdriver_state = 0
                 self.zoom_state = 0
-
+        elif self.zoom_state == 3:
+            if (0 < x < 1024) and (0 < y < 768):
+                self.zoom_state = 0
     def draw(self):
         self.screen.blit(self.img_room, (0,0))
         if self.zoom_state == 1:
             self.screen.blit(self.zoom_under_bed, (0,0))
         elif self.zoom_state == 2:
-            self.screen.blit(self.zoom_key, (0,0))
+            self.screen.blit(self.zoom_key, (0, 0))
+        elif self.zoom_state ==3:
+            self.screen.blit(self.zoom_toilet, (0, 0))
