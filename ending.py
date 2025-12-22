91c6782d5fad8fde02cfaa16cc501daac544912e
P4skql <t23j177oc@ous.jp>
2025-12-22 16:07:40 +0900

    end

--- ending.py
+++ ending.py
@@ -4,36 +4,26 @@ Created on Wed Dec  9 10:23:51 2020
 
 @author: Ozaki
 """
-
+import pygame
 from room_base import Room_Base
 
 class Ending(Room_Base):
     def __init__(self, screen, lock_flag, item_get, item_use):
+        super().__init__(screen, lock_flag, item_get, item_use)
         self.screen = screen
         self.lock_flag = lock_flag
         self.item_get = item_get
         self.item_use = item_use
+        self.start = pygame.image.load("res/end/end.png")
+        self.next_room = 6
+        self.zoom_state = 0
     
-    
-    def pos_event(self, x, y):
-        pass
-    
-    
+
     def click_event(self, x, y):
         pass
     
-    
-    def key_event(self, key):
-        pass
-    
-    
-    def next_state(self):
-        return 5
-    
-    
-    def do(self):
-        pass
-    
-    
     def draw(self):
-        pass
+        self.screen.blit(self.end, (0, 0))
+
+    def next_state(self):
+       pass
