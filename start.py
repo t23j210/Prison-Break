989b12c51b3f4d884a331364700c15ef8ce3e261
t23j210 <t23j210ol@ous.jp>
2025-12-26 02:30:56 +0900

    music

--- start.py
+++ start.py
@@ -15,6 +15,10 @@ class Start(Room_Base):
         self.item_get = item_get
         self.item_use = item_use
         self.start = pygame.image.load("res/start/start.png")
+        pygame.mixer.init(frequency = 44100)
+        pygame.mixer.music.load("res/music/music.mp3")
+        pygame.mixer.music.set_volume(0.2)
+        self.play_music = False
         self.next_room = 0
         self.zoom_state = 0
     
@@ -23,9 +27,15 @@ class Start(Room_Base):
         if self.zoom_state == 0:
             if (373 < x < 661) and (476 < y < 561):
                 self.next_room = 1
-    
+                pygame.mixer.music.stop()
+
     def draw(self):
         self.screen.blit(self.start, (0, 0))
+
+    def do(self):
+        if self.play_music == False:
+            pygame.mixer.music.play(-1)
+            self.play_music = True
 
     def next_state(self):
         next = self.next_room #次の部屋
