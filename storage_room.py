65085b7574a8a90a5f3fac927bf5c5affc6b0804
t23j210 <t23j210ol@ous.jp>
2025-12-31 01:21:47 +0900

    storage_room

--- storage_room.py
+++ storage_room.py
@@ -17,16 +17,21 @@ class Storage_room(Room_Base):
         self.zoom_door2 = pygame.image.load("res/storage_room/zoom_door_no.png") #鍵なし扉
         self.item_flashlight = pygame.image.load("res/storage_room/item_flashlight.png") #懐中電灯
         self.item_page3 = pygame.image.load("res/storage_room/item_page3.png") #ページ3
+        self.img_look = pygame.image.load("res/jail/item_diary.png")
         self.zoom_state = 0
         self.item_sdriver_state = 0
         self.next_room = 4
         
     def click_event(self, x, y):
+        if self.look_state == 1:
+            return
+        
         if self.zoom_state == 0: #初期位置
             if (490 < x < 615) and (260 < y < 390):
-                self.zoom_state = 1
                 if self.lock_flag[4] == True:
                     self.zoom_state = 3
+                else:
+                    self.zoom_state = 1
             elif (703 < x < 829) and (578 < y < 612): #懐中電灯を入手
                 self.item_sdriver_state = 1
                 self.item_get[8] = True
@@ -53,38 +58,42 @@ class Storage_room(Room_Base):
                 if self.item_use == [False, True, False, False, False, False, False, False, False, False, False, False, False, False, False]:
                     if (475 < x < 570) and (370 < y < 450):
                         self.lock_flag[4] = True
+                        self.se[8].play()
                         self.item_use[1] = False
                         self.zoom_state = 3
-            elif (240 < x < 780) and (43 < y < 634):
-                self.zoom_state = 1
-            else:
-                if self.item_get[8] == False:
-                    self.zoom_state = 0
-                else:
+            if not (170 < x < 850) and (0 < y < 640):
+                if self.item_get[8] == True:
                     self.zoom_state = 2
+                else:
+                    self.zoom_state = 0
         elif self.zoom_state == 2: #懐中電灯なしver
             if (490 < x < 615) and (260 < y < 390):
-                self.zoom_state = 1
                 if self.lock_flag[4] == True:
                     self.zoom_state = 3
+                else:
+                    self.zoom_state = 1    
             elif (840 < x < 918) and (420 < y < 455): #ページ3を入手
                 self.item_sdriver_state = 2
                 self.item_get[9] = True
                 self.se[0].play()
-                if self.item_get[8] == False:
-                    self.zoom_state = 0
-                else:
+                if self.item_get[8] == True:
                     self.zoom_state = 2
+                else:
+                    self.zoom_state = 0
             elif (338 < x < 400) and (123 < y < 152):
                 self.next_room = 2
             elif (566 < x < 602) and (190 < y < 226):
                 self.next_room = 3       
             else:
                 self.zoom_state = 2
-        elif self.zoom_state ==3:
+        elif self.zoom_state == 3:
             if (490 < x < 615) and (260 < y < 390):
                 self.next_room = 5
-        
+            if not (170 < x < 850) and (0 < y < 640):
+                if self.item_get[8] == True:
+                    self.zoom_state = 2
+                else:
+                    self.zoom_state = 0
         
     def draw(self):
         self.screen.blit(self.img_room1, (0,0)) #初期位置
@@ -94,6 +103,9 @@ class Storage_room(Room_Base):
             self.screen.blit(self.img_room2, (0,0)) #懐中電灯なしver
         elif self.zoom_state == 3:
             self.screen.blit(self.zoom_door2, (0,0)) #鍵なし扉
+
+        if self.look_state == 1:
+            self.screen.blit(self.img_look, (0, 0))     
             
     def next_state(self):
         next = self.next_room
@@ -102,3 +114,4 @@ class Storage_room(Room_Base):
     
     def next_music(self):
         return 4
+    
